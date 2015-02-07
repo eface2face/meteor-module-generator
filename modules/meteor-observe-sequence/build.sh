a7ffc0f9240ad3de4614a9c35ab00bf48bcc5a25
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2015-02-07 19:22:39 +0100

    remove dependency from minimongo

--- modules/meteor-observe-sequence/build.sh
+++ modules/meteor-observe-sequence/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 cat module.requires > dist/observe_sequence.js
+cat ../../meteor/packages/minimongo/diff.js >> dist/observe_sequence.js
+cat ../../meteor/packages/minimongo/objectid.js >> dist/observe_sequence.js
 cat ../../meteor/packages/observe-sequence/observe_sequence.js >> dist/observe_sequence.js
 cat module.exports >> dist/observe_sequence.js
 
